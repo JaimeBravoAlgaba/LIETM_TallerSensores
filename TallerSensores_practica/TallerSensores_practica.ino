3c027bf4f9ddd6ec2f7f4987a45abb5600fc98c2
JaimeBravoAlgaba <jaime.bravo.algaba@gmail.com>
2024-11-28 13:23:49 +0100

    Versión taller

--- TallerSensores_practica/TallerSensores_practica.ino
+++ TallerSensores_practica/TallerSensores_practica.ino
@@ -1,7 +1,13 @@
+// Declaración de pines
 const int potPin = A0;
 const int ledPin = 9;
 
-int lectura_min = 0, lectura_max = 1023;
+// Variables eléctricas
+float Rf_min = 29.0, Rf_max = 90, R = 34.0;
+
+// Rangos de lectura
+int lectura_min = R/(R+Rf_max) * 1023;
+int lectura_max = R/(R+Rf_min) * 1023;
 int salida_min = 0, salida_max = 255;
 
 void setup() {
@@ -11,13 +17,22 @@ void setup() {
 }
 
 void loop() {
+  // Medición: tomar el dato "crudo" del sensor
   int potValue = analogRead(potPin);
+
+  // Ajuste de escala: traducir el dato crudo al rango que deseemos
   potValue = map(potValue, lectura_min, lectura_max, salida_min, salida_max);
+
+  // Saturación: se asegura que la señal no exceda el rango de 0 a 255 para el PWM
+  potValue = (potValue>255)? 255 : potValue;
+  potValue = (potValue<0)? 0 : potValue;
+
+  // Iluminar el LED y graficar
+  analogWrite(ledPin, potValue);
   
   Serial.print(salida_min); Serial.print(",");
   Serial.print(salida_max); Serial.print(",");
   Serial.println(potValue);
 
-  analogWrite(ledPin, potValue);
   delay(5);
 }
